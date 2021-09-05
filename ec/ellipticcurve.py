688c73d61fb9530e8537986ff42127a71a040a8e
Trevor Tomlin <32802101+trevortomlin@users.noreply.github.com>
2021-09-05 10:43:30 -0700

    Added more functions for points.

--- ec/ellipticcurve.py
+++ ec/ellipticcurve.py
@@ -1,22 +1,48 @@
 import math
 
 #Curve25519 = y^2 = x^3 + 48662x^2 + x
-#Coefficients are stored [x, x^2, ..., x^n]
-CURVE_COEFFICIENTS = [1, 48662, 1]
-
+#Coefficients are stored [b, x, x^2, ..., x^n]
+#CURVE_COEFFICIENTS = [0, 1, 48662, 1]
+CURVE_COEFFICIENTS = [1, -1, 0, 1]
+#P = 2^255-19
+P = 97
 
 class Point:
 
-	def __init__(self, x, y):
+	def __init__(self, x, y, curve):
 
 		self.x, self.y = x, y
+		self.curve = curve
+
+	def __mul__(self, n: int):
+		pass
+
+	def double(self):
+		l = self.curve.tangent()
+		x = s**2 - self.x - b.x
+		y = -1 * self.y + s * (self.x - x)
+		r = Point(x, y, self.curve)
+
+	def __neg__(self):
+		n = Point(x, -y)
+		return n
 
 	def __add__(self, b):
+
+		if self.x == b.x: 
+			if self.y == -b.y:
+				return 0
+			elif self.y == b.y:
+				return 0
+
 		s = Curve.secant(self, b)
-		x = s**2 - 2.0 * self.x
-		y = self.x + s * (x - self.x)
-		r = Point(x, y)
+		x = s**2 - self.x - b.x
+		y = -1 * self.y + s * (self.x - x)
+		r = Point(x, y, self.curve)
 		return r
+
+	def __sub__(self, b):
+		return self + -b
 
 	def __str__(self):
 		return "(" + str(self.x) + ", " + str(self.y) + ")"
@@ -33,9 +59,14 @@ class Curve:
 
 		for index in range(len(self.coefficients)):
 
-			y2 += self.coefficients[index] * x ^ index+1
+			y2 += self.coefficients[index] * x ^ index
 
 		return math.sqrt((y2 % self.p)) * sign
+
+	def tangent(self, a: Point) -> float:
+
+		t = (3 * a.x**2 + self.coefficients[1]) / (2 * a.y)
+		return t
 
 	@staticmethod
 	def secant(a: Point, b: Point) -> float:
@@ -48,27 +79,23 @@ class Curve:
 
 def main():
 
-	ec_curve = Curve(CURVE_COEFFICIENTS, 2^255-19)
+	ec_curve = Curve(CURVE_COEFFICIENTS, P)
 	
 	x = 1
 	y = ec_curve.evaluate(x, 1)
 
-	p = Point(x, y)
+	p = Point(x, y, ec_curve)
 
 	x1 = 2
 	y1 = ec_curve.evaluate(x1, -1)
 
-	q = Point(x1, y1)	
+	q = Point(x1, y1, ec_curve)
 
-	#print(y, y1)
-	#print(Curve.secant(p, q))
+	print(Curve.secant(p, q))
 
 	r = p + q
 
 	print(p, q, r)
-	#print(p + q + r)
-
-	
 
 if __name__ == "__main__":
 	main()
